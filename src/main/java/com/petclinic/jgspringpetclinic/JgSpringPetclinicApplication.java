a1188183fbd37b3e8a5eea5b0fc5045e47739d9e
jerzykGr <jerzyk.gr@gmail.com>
2018-09-18 19:07:54 +0200

    initial

---
+++ src/main/java/com/petclinic/jgspringpetclinic/JgSpringPetclinicApplication.java
@@ -0,0 +1,12 @@
+package com.petclinic.jgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JgSpringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JgSpringPetclinicApplication.class, args);
+    }
+}
